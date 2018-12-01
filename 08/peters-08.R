4b73144b85b658808ec0e9f3a00928417412a943
cxan96 <c.p.xanten@gmail.com>
2018-12-01 23:45:13 +0100

    finished no 2

--- 08/peters-08.R
+++ 08/peters-08.R
@@ -65,13 +65,17 @@ quadratic_interpolation <- function(f, left, right, tol=1e-4) {
 test_quadratic_interpolation <- function() {
   test_that("Test of the quadratic interpolation algorithm.", {
     # simple quadratic function
-    expect_equal(quadratic_interpolation(function(x) x**2, -1, 1), 0, tolerance=1e-4)
+    expect_equal(quadratic_interpolation(function(x) x**2, -1, 1), 0,
+                 tolerance=1e-4)
     # shifted 4'th degree polynomial
-    expect_equal(quadratic_interpolation(function(x) (x-3)**4 + 1, 1, 4), 3, tolerance=1e-4)
+    expect_equal(quadratic_interpolation(function(x) (x-3)**4 + 1, 1, 4), 3,
+                 tolerance=1e-4)
     # find pi/2
-    expect_equal(quadratic_interpolation(function(x) -sin(x), 1, 2), pi/2, tolerance=1e-4)
+    expect_equal(quadratic_interpolation(function(x) -sin(x), 1, 2), pi/2,
+                 tolerance=1e-4)
     # additional logarithm
-    expect_equal(quadratic_interpolation(function(x) log((x+2)**2)+3, -10, 10), -2, tolerance=1e-4)
+    expect_equal(quadratic_interpolation(function(x) log((x+2)**2)+3, -10, 10), -2,
+                 tolerance=1e-4)
   })
 }
 
@@ -81,14 +85,17 @@ test_quadratic_interpolation()
 # ======
 
 # a)
-
 plotMedianProblem <- function() {
+  # init values
   a <- c(15, 51, 33, 20, 66, 35, 72, 3, 34)
+  # init 'search space'
   x <- seq(0, 100, 1)
   print(
-    ggplot(data.frame(x=x, y=sapply(x, function(beta) sum(abs(a-beta)))), aes(x=x, y=y)) +
+    # plot the optimization problem
+    ggplot(data.frame(x=x, y=sapply(x, function(beta) sum(abs(a-beta)))),
+           aes(x=x, y=y)) +
       geom_point() +
-      ggtitle("Median calculation by optimizing the sum of absolute errors") +
+      ggtitle("Median calculation by optimizing the sum of the absolute errors") +
       scale_x_continuous("beta", breaks=2*0:50) +
       scale_y_continuous("Sum of the absolute errors") +
       theme(plot.title = element_text(hjust = 0.5))
@@ -98,6 +105,108 @@ plotMedianProblem <- function() {
 plotMedianProblem()
 
 # As we can see, the median is 34 because it minimizes the sum of the absolute errors.
+
+# b)
+# Iteration: 1
+# left = min(a) = 3
+# right = max(a) = 72
+# best = 3 + phi * (72 - 3) = 45.642
+# new = 3 + 72 - 45.642 = 29.358
+# SWAP:
+# best = 29.358
+# new = 45.642
+# ADJUST BORDERS:
+# right = 45.642
+#
+# Iteration: 2
+# new = 3 + 45.642 - 29.358 = 19.284
+# SWAP:
+# -
+# ADJUST BORDERS:
+# left = 19.284
+#
+# Iteration: 3
+# new = 19.284 + 45.642 - 29.358 = 35.568
+# SWAP:
+# best = 35.568
+# new = 29.358
+# ADJUST BORDERS:
+# left = 29.358
+#
+# Iteration: 4
+# new = 29.358 + 45.642 - 35.568 = 39.432
+# SWAP:
+# -
+# ADJUST BORDERS:
+# right = 39.432
+#
+# Iteration: 5
+# new = 29.358 + 39.432 - 35.568 = 33.222
+# SWAP:
+# best = 33.222
+# new = 35.568
+# ADJUST BORDERS
+# right = 35.568
+#
+# Iteration: 6
+# new = 29.358 + 35.568 - 33.222 = 31.704
+# SWAP:
+# -
+# ADJUST BORDERS:
+# left = 31.704
+#
+# Iteration: 7
+# new = 31.704 + 35.568 - 33.222 = 34.05
+# SWAP:
+# best = 34.05
+# new = 33.222
+# ADJUST BORDERS
+# left = 33.222
+#
+# Iteration: 8
+# new = 33.222 + 35.568 - 34.05 = 34.74
+# SWAP:
+# -
+# ADJUST BORDERS
+# right = 34.74
+#
+# DONE! left = 33.222 <= MEDIAN <= right = 34.74
+# => MEDIAN = 34
+
+# c)
+# Assumption: All the elements x_i are uniformly distributed in the interval
+# [min(x), max(x)].
+# Under this assumption, the GSS should terminate as soon as
+# right-left <= (max(x)-min(x))/n, where n is the number of elements in x.
+#
+# In each step of GSS, the interval size is shrinked by the factor phi=0.618,
+# which yields the following equation: (max - min)*phi^k <= (max-min)/n, where k
+# is the number of iterations after which the GSS terminates.
+#
+# Solving this for k yields: k <= -ln(n)/ln(phi) = 2.078 * ln(n) which means
+# that the number of iterations of the golden section median search is in O(ln(n)).
+# So, on average GSS terminates after 2.078 * ln(n) iterations, assuming all
+# the elements in x are uniformly distributed.
+
+# d)
+# When using GSS to compute the median, the algorithm terminates after
+# O(ln(n)) iterations. It should be noted however, that in every single iteration
+# the sum of the absolute errors has to be computed, which requires looping
+# through the whole dataset once. Thus, the total complexity of the GSS
+# median algorithm is O(n*ln(n)), just like an efficient sorting algorithm.
+#
+# In GSS, the number of iterations is only in O(ln(n)), if we can assume that
+# the data is uniformly distributed. This means that in the worst case,
+# GSS can be worse than O(n*ln(n)). Taking this into consideration, I would
+# prefer using the sorting method, because it is more robust than the
+# GSS method and always delivers its result in O(n*ln(n)), provided an
+# efficient sorting algorithm is employed.
+
+# e)
+# In the median function of R, the first half of the data is partially sorted
+# and then the mean of the two biggest elements of that half is retured.
+# This is more intelligent than sorting the whole vector,
+# because only half of the data has to be sorted.
 
 # No. 3)
 # ======
