2c1cb878309acfe7a192a81d70269538c51c61c1
cxan96 <c.p.xanten@gmail.com>
2018-12-01 17:22:37 +0100

    implemented the coordinate descent algorithm

--- 08/peters-08.R
+++ 08/peters-08.R
@@ -1,6 +1,8 @@
 # Name: Christian Peters
 
 library(testthat)
+library(ggplot2)
+library(mvtnorm)
 
 # No. 1)
 # ======
@@ -10,10 +12,12 @@ library(testthat)
 #' @param f     A function that is to be minimized.
 #' @param left  The left endpoint of the search interval. It must be numeric.
 #' @param right The right endpoint of the search interval. It must be numeric.
-#' @param eps   Tolerance threshold. Used to adjust the precision of the optimizer.
-quadratic_interpolation <- function(f, left, right, eps=1e-4) {
+#' @param tol   Tolerance threshold. Used to adjust the precision of the optimizer.
+#' 
+#' @return The arguments of f for which f is minimal in the interval [left, right]
+quadratic_interpolation <- function(f, left, right, tol=1e-4) {
   # type checks
-  stopifnot(is.function(f), is.numeric(c(left, right)), left<=right, is.numeric(eps))
+  stopifnot(is.function(f), is.numeric(c(left, right)), left<=right, is.numeric(tol))
   
   # calculate initial values
   best <- (left+right)/2
@@ -25,7 +29,7 @@ quadratic_interpolation <- function(f, left, right, eps=1e-4) {
                                                     f_best*(right-left) + 
                                                     f_left * (best-right))
   # iterate until the desired precision is reached
-  while (abs(right - left) > eps) {
+  while (abs(right - left) > tol) {
     f_new <- f(new)
     if (f_new < f_best) {
       tmp <- new
@@ -72,3 +76,122 @@ test_quadratic_interpolation <- function() {
 }
 
 test_quadratic_interpolation()
+
+# No. 2)
+# ======
+
+# a)
+
+plotMedianProblem <- function() {
+  a <- c(15, 51, 33, 20, 66, 35, 72, 3, 34)
+  x <- seq(0, 100, 1)
+  print(
+    ggplot(data.frame(x=x, y=sapply(x, function(beta) sum(abs(a-beta)))), aes(x=x, y=y)) +
+      geom_point() +
+      ggtitle("Median calculation by optimizing the sum of absolute errors") +
+      scale_x_continuous("beta", breaks=2*0:50) +
+      scale_y_continuous("Sum of the absolute errors") +
+      theme(plot.title = element_text(hjust = 0.5))
+  )
+}
+
+plotMedianProblem()
+
+# As we can see, the median is 34 because it minimizes the sum of the absolute errors.
+
+# No. 3)
+# ======
+
+#' Implementation of the coordinate descent minimization algorithm.
+#' 
+#' @param f       The function to minimize.
+#' @param start   The starting value.
+#' @param tol     The minimal step difference. Used as a stopping condition.
+#' @param maxIter The maximum number of iterations.
+#' 
+#' @return A local minimum of f.
+coordinateDescent <- function(f, start, tol=1e-4, maxIter=20) {
+  # dimensionality of the search space
+  n <- length(start)
+  # iteration counter
+  curIter <- 1
+  
+  # initialize starting values
+  last <- start
+  last_stopCondition <- last
+  f_last <- f(last)
+  
+  # internal value used to locate minima along coordinate axes
+  eps <- 1e-6
+  repeat {
+    # determine k'th unit vector
+    k <- 1 + curIter %% n
+    e_k <- rep(0, n)
+    e_k[k] <- 1
+    
+    # helper function that determines the endpoint of the search interval along
+    # the current unit vector e_k where the sign of direction indicates the
+    # search direction (either positive or negative)
+    getIntervalEndpoint <- function(direction) {
+      curIteration <- 1
+      repeat {
+        curEndpoint <- curIteration * sign(direction)
+        f_new <- f(last + curEndpoint * e_k)
+        f_test <- f(last + (curEndpoint - eps * sign(direction)) * e_k)
+        if (f_new > f_test) {
+          return(curEndpoint)
+        }
+        # increase the search distance exponentially
+        curIteration <- curIteration * 2
+      }
+    }
+    
+    # minimize f along the vector
+    if (f(last + eps * e_k) > f_last) {
+      right <- 0
+      left <- getIntervalEndpoint(-1)
+    } else {
+      left <- 0
+      right <- getIntervalEndpoint(1)
+    }
+    
+    # get the optimal step size along the current coordinate by using quadratic
+    # interpolation
+    ny <- quadratic_interpolation(function(ny) f(last + ny * e_k), left, right)
+    last <- last + ny * e_k
+    f_last <- f(last)
+    
+    # check for stop condition once all coordinate directions have been updated
+    # once.
+    if (curIter %% n == 0) {
+      # stop if the last (complete) step was sufficiently small
+      if (norm(last - last_stopCondition, type="2") < tol) {
+        return(last)  
+      }
+      
+      last_stopCondition <- last
+    }
+    # also stop if the maximum number of iterations was reached
+    if (curIter >= maxIter) {
+      return(last)
+    }
+
+    curIter <- curIter + 1
+  }
+}
+
+# This function is used to test the coordinate descent algorithm.
+test_coordinateDescent <- function() {
+  test_that('Test of the coordinate descent algorithm.', {
+    # simple one dimensional function
+    expect_equal(coordinateDescent(function(x) (x-3)**2+1, 0), 3, tolerance=1e-4)
+    # negative bivariate normal distribution with rho > 0
+    expect_equal(coordinateDescent(function(x) -dmvnorm(x, c(1, 2),
+                                                        matrix(c(1, 0.5, 0.5, 1),
+                                                               nrow=2)),
+                                   c(0, 0)),
+                 c(1, 2), tolerance=1e-4)
+  })
+}
+
+test_coordinateDescent()
